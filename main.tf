96f34de8a1639a063e2bfb980823c1c4aab89370
Mohamed Attia <85102022+Attiathedragon@users.noreply.github.com>
2023-09-01 01:02:27 +0300

    ddd

--- main.tf
+++ main.tf
@@ -70,3 +70,53 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
+
+
+#creat new instance and assign it to Attia vpc 
+
+resource "aws_instance" "attia" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["attia]
+
+  tags = {
+    Name = "Attia vpc for Terraform"
+  }
+}
+
+resource "aws_security_group" "attia" {
+  name = "attia"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = [attia]
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = [attia]
+}
+
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = [attia]
+}
+
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = [attia]
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+
+output "instance_ami" {
+  value = aws_instance.attia.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.attia.arn
+}
